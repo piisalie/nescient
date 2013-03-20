5c3c1dd81ccee4c207350b953510e08e4a0392b6
Paul Dawson <excidere@gmail.com>
2013-03-20 07:45:49 -0500

    switched to using match() and fixed regex (again)

--- lib/nescient/message.rb
+++ lib/nescient/message.rb
@@ -1,17 +1,25 @@
+# -*- coding: utf-8 -*-
 module Nescient
   class Message
-    PARSE = /\A(:(?<prefix>\S+)\s)?
-                 (?<command>\S+)
-                 (?:\s(?<params>[^:\s]+))*
-                 (?:\s:(?<trailing>.+))?/x
+    PARSE = /\A(?::(?<prefix>\S+)\s)?
+                   (?<command>\S+)
+                   (?:\s(?<params>[^:]+))?\s+
+                   (?::(?<trailing>.+))?/x
     attr_reader :prefix, :command, :params, :trailing
 
     def initialize(line)
       @string   = line.to_s
-      @prefix   = @string[PARSE, :prefix]
-      @command  = @string[PARSE, :command]
-      @params   = @string[PARSE, :params]
-      @trailing = @string[PARSE, :trailing]
+      parse
     end
+
+    def parse
+      if (pieces = PARSE.match(@string))
+        @prefix   = pieces[:prefix]
+        @command  = pieces[:command]
+        @params   = pieces[:params]
+        @trailing = pieces[:trailing]
+      end
+    end
+    
   end
 end
