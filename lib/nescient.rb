ecf3b8ab94ff2a0539f8f3448095fa80979a1617
James Edward Gray II <james@graysoftinc.com>
2013-03-26 18:41:55 -0500

    Moving the top-level code into its own namespace.  Call with:  Nescient.run

--- lib/nescient.rb
+++ lib/nescient.rb
@@ -3,9 +3,10 @@ require_relative "nescient/bot"
 require_relative "nescient/message"
 require_relative "nescient/loginaction"
 
-
-def run
-  bot = Nescient::Bot.new("irc.freenode.net", 6667)
-  bot.listen_for = Nescient::LoginAction.new
-  bot.listen
+module Nescient
+  def self.run
+    bot            = Bot.new("irc.freenode.net", 6667)
+    bot.listen_for = LoginAction.new
+    bot.listen
+  end
 end
