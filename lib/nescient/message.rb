80e7981670ef17798beef7222b0bb68fc9a623a0
James Edward Gray II <james@graysoftinc.com>
2013-03-26 18:47:34 -0500

    Removing unneeded details from Message.

--- lib/nescient/message.rb
+++ lib/nescient/message.rb
@@ -2,35 +2,37 @@ require 'strscan'
 
 module Nescient
   class Message
-    attr_reader :prefix, :command, :params, :trailing
+    attr_reader :prefix, :command, :params
 
     def initialize(line)
-      @scanner = StringScanner.new(line.to_s)
-      parse(@scanner)
+      @prefix  = nil
+      @command = nil
+      @params  = [ ]
+
+      parse(StringScanner.new(line.to_s))
     end
 
     def parse(scanner)
       parse_prefix(scanner)
       parse_command(scanner)
       parse_params(scanner)
-      @trailing = @params.last
     end
 
     def parse_prefix(scanner)
-      scanner.scan(/:(\S+)\s+/)
-      @prefix = scanner[1]
+      if scanner.scan(/:(\S+)\s+/)
+        @prefix = scanner[1]
+      end
     end
 
     def parse_command(scanner)
-      scanner.scan(/([0-9a-zA-Z]+)\s*/)
-      @command = scanner[1]
+      if scanner.scan(/([0-9a-zA-Z]+)\s*/)
+        @command = scanner[1]
+      end
     end
 
     def parse_params(scanner)
-      @params = [ ]
       while param = scanner.scan(/([^:\s]\S*)\s+|:([^\r\n]+)\r?\n?\z/)
-        param = scanner[1] || scanner[2]
-        @params << param
+        @params << scanner[1] || scanner[2]
       end
     end
   end
